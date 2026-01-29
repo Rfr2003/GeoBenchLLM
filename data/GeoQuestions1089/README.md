b97fd85d025bbc7667acca0961f5c5b303eb0b9a
Rodrigo FERREIRA RODRIGUES <rferreir@occidata-cluster.irit.fr>
2026-01-29 16:13:25 +0100

    Adding GeoQuestions1089

---
+++ data/GeoQuestions1089/README.md
@@ -0,0 +1,57 @@
+# GeoQuestions1089
+
+GeoQuestions1089 is a geospatial question-answering dataset targeting the Knowledge Graph YAGO2geo. It contains 1089 triples of geospatial questions, their answers, and the respective SPARQL/GeoSPARQL queries.
+
+Their repository can be found [here](https://github.com/AI-team-UoA/GeoQuestions1089). 
+
+## Download the Data
+
+The dataset is divided into two files : [GeoQuestions1089.json](https://github.com/AI-team-UoA/GeoQuestions1089/blob/main/GeoQuestions1089.json) and [GeoQuestions1089_answers.json](https://github.com/AI-team-UoA/GeoQuestions1089/blob/main/GeoQuestions1089_answers.json). Download them.
+
+Then, we will also need the union of the Yago2 + Yago2Geo knowledge graph. The authors gave a link to download it, however the Yago2Geo files couldn't be imported into GraphDB (used to query the graph, download it [here](https://graphdb.ontotext.com/)). We then download it from the knowlegde graph [homepage](https://yago2geo.di.uoa.gr/).
+
+Our advise is to :
+- Download the graph from [here](https://yago2geo.di.uoa.gr/data/yago2_plus_geo.zip).
+- Only keep the file **yago-2_ascii.nt**, discard the others.
+- Download the content of Yago2geo for the 4 existing countries (Greece, Ireland, UK and USA) from [here](https://yago2geo.di.uoa.gr/).
+- Put all the files in the same folder with **yago-2_ascii.nt**.
+
+Then, you can run this command to import everything into GraphDB :
+
+```bash
+importrdf load --force -c PATH_TO_CONGIG_FILE.ttl -m parallel PATH_TO_THE_FOLDER_CONTAINING_THE_DATA
+```
+
+We also gave you an example for the configuration file for setting up GraphDB.
+
+## Processing the data
+
+First, run in parallel GraphDB.
+
+Once it's running, you can run the script **code/extract.py** to obtain the dataset with cleaned answers in a .csv format.
+
+```bash
+python3 code/extract.py --output_dir 'PATH_TO_THE_OUTPUT_DIR' --data_dir 'PATH_TO_THE_DIR_CONTAINING_THE_DATA' --db_url 'http://localhost:7200/repositories/NAME_OF_THE_GRAPHDB_REPO'
+```
+
+Note that the __data_dir__ must contain the files : **GeoQuestions1089.json** and **GeoQuestions1089_answers.json**.
+
+## Converting to HF dataset format
+
+Run the notebook **code/create_dataset.ipynb**, to have the Hugging Faces dataset. We also do some more processing in it as translating some places names from Greek to English and converting some values in square degrees to square kilometers.
+
+## Citation
+
+Thanks to the authors for their work. If you use this dataset, please cite their paper :
+
+```Tex
+@inproceedings{10.1007/978-3-031-47243-5_15,
+  title = {Benchmarking Geospatial Question Answering Engines Using the Dataset GeoQuestions1089},
+  author = {Sergios-Anestis Kefalidis, Dharmen Punjani, Eleni Tsalapati, 
+         Konstantinos Plas, Mariangela Pollali, Michail Mitsios, 
+         Myrto Tsokanaridou, Manolis Koubarakis and Pierre Maret},
+  booktitle = {The Semantic Web - {ISWC} 2023 - 22nd International Semantic Web Conference,
+            Athens, Greece, November 6-10, 2023, Proceedings, Part {II}},
+  year = {2023}
+}
+```
---
+++ data/GeoQuestions1089/code/extract.py
@@ -0,0 +1,256 @@
+from enum import Enum
+import pandas as pd
+import json
+import re
+from shapely import wkt
+from shapely.ops import transform
+from pyproj import Transformer
+import numpy as np
+from shapely.geometry import GeometryCollection
+from enum import Enum
+from collections import Counter
+from tqdm import tqdm
+from SPARQLWrapper import SPARQLWrapper, JSON
+import argparse
+import os
+
+first_words = []
+
+class AnswerType(Enum):
+    NUMBER = 0
+    URL = 1
+    POLYGON = 2
+    STR = 3
+    OTHER = 4
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str, required=True, help="Path to the directory containing GeoQuestions1089.json and GeoQuestions1089_answers.json")
+    parser.add_argument("--output_dir", type=str, required=True, help="Path to the directory where to save the data")
+    parser.add_argument("--db_url", type=str, required=True, help="URl where the database is listening")
+    
+    args = parser.parse_args()
+    return args
+
+def load_json(file_name):
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    return list(data.values())
+
+def polygon_to_coordinates(text):
+
+    text = text.strip()
+
+    match = re.search(r'(POLYGON\s*\(\(.*?\)\)|MULTIPOLYGON\s*\(\(\(.*?\)\)\))', text, re.DOTALL)
+    if not match:
+        return []
+
+    geom = match.group(0)
+    geom = wkt.loads(geom)
+
+    centroid = geom.centroid
+    return [(centroid.y, centroid.x)] #(lat, long)
+
+def detect_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+        
+def detect_type(text):
+    
+    url_pattern = re.compile(
+        r'^(https?|ftp)://[^\s/$.?#].[^\s]*$',
+        re.IGNORECASE
+    )
+    
+    if detect_number(text.strip()):
+        return AnswerType.NUMBER 
+    elif url_pattern.match(text.strip()):
+        return AnswerType.URL
+    elif text.strip().isalnum():
+        return AnswerType.STR
+    elif re.search(r'POLYGON', text.strip(), re.IGNORECASE):
+        return AnswerType.POLYGON
+    else:
+        return AnswerType.OTHER
+
+def clean_url(url, db_url):
+    stopwords = ["geoentity", "wikicategory", "osientity", "osentity", "wordnet"]
+    entity = url.split('/')[-1]
+    # Greek entities
+    if "gagentity" in entity:
+        sparql = SPARQLWrapper(db_url)
+        sparql.setQuery(f"""
+                PREFIX y2geoo: <http://kr.di.uoa.gr/yago2geo/ontology/>
+                
+                SELECT ?name
+                WHERE {{
+                    <{url}> y2geoo:hasGAG_Name ?name .
+                }}
+                """)
+        sparql.setReturnFormat(JSON)
+        
+        results = sparql.query().convert()
+        for r in results["results"]["bindings"]:
+            entity = r["name"]["value"]
+            
+    #Ireland entities
+    elif "osientity" in entity:
+        sparql = SPARQLWrapper(db_url)
+        sparql.setQuery(f"""
+                PREFIX y2geoo: <http://kr.di.uoa.gr/yago2geo/ontology/>
+                
+                SELECT ?name
+                WHERE {{
+                    <{url}> y2geoo:hasOSI_Name ?name .
+                }}
+                """)
+        sparql.setReturnFormat(JSON)
+        
+        results = sparql.query().convert()
+        for r in results["results"]["bindings"]:
+            entity = r["name"]["value"]
+            
+    #USA entities
+    elif "osnientity" in entity:
+        sparql = SPARQLWrapper(db_url)
+        sparql.setQuery(f"""
+                PREFIX y2geoo: <http://kr.di.uoa.gr/yago2geo/ontology/>
+                
+                SELECT ?name
+                WHERE {{
+                    <{url}> y2geoo:hasOSNI_Name ?name .
+                }}
+                """)
+        sparql.setReturnFormat(JSON)
+        
+        results = sparql.query().convert()
+        for r in results["results"]["bindings"]:
+            entity = r["name"]["value"]
+
+    #UK entities
+    elif "osentity" in entity:
+        sparql = SPARQLWrapper(db_url)
+        sparql.setQuery(f"""
+                PREFIX y2geoo: <http://kr.di.uoa.gr/yago2geo/ontology/>
+                
+                SELECT ?name
+                WHERE {{
+                    <{url}> y2geoo:hasOS_Name ?name .
+                }}
+                """)
+        sparql.setReturnFormat(JSON)
+        
+        results = sparql.query().convert()
+        for r in results["results"]["bindings"]:
+            entity = r["name"]["value"]
+    else:
+        split = entity.split('_')
+        first_words.append(split[0])
+        if split[0] in stopwords or re.match(r".*entity$", split[0], re.IGNORECASE):
+            split = split[1:]
+        if split[-1].isnumeric():
+            split = split[:-1]
+        entity = " ".join(split)
+    return entity
+
+def clean_answer(answer, db_url):
+    replicate = False
+    a_type = None
+    if answer == None:
+        return [], replicate, None
+    if isinstance(answer, dict):
+        final_answer = [answer['boolean']]
+        a_type = ['bool']
+    elif detect_type(answer) == AnswerType.POLYGON:
+        final_answer = polygon_to_coordinates(answer)
+        a_type = ['coord']
+    else:
+        # get rid of GeoQuestions_w (same triples than the others but with mistakes)
+        pattern = re.compile(r"same-as:(\d+)")
+        matches = pattern.findall(answer)
+        if matches:
+            final_answer = matches[0].split(":")[-1]
+            replicate = True
+        else:
+            final_answer = []
+            a_type = []
+            lines = answer.splitlines()[1:]
+            for line in lines:
+                line = line.replace('"', "")
+                for l in line.split(','):
+                    detected_type = detect_type(l)
+                    if detected_type == AnswerType.URL:
+                        new_answer = clean_url(l, db_url)
+                        a_type.append('str')
+                    elif detected_type == AnswerType.NUMBER:
+                        new_answer = float(l)
+                        a_type.append('numeric')
+                    elif detected_type == AnswerType.STR:
+                        new_answer = l
+                        a_type.append('str')
+                    else:
+                        new_answer = None
+                    if new_answer:
+                        final_answer.append(new_answer)
+    return final_answer, replicate, a_type
+
+def clean_answers(answers, db_url):
+    clean = []
+    replicates = []
+    a_types = []
+    for answer in tqdm(answers):
+        a, replicate, a_type = clean_answer(answer, db_url)
+        clean.append(a)
+        replicates.append(replicate)
+        a_types.append(a_type)
+    return clean, replicates, a_types
+
+def create_csv(questions, answers, output_dir, db_url):
+    cleaned_answers, replicates, a_types = clean_answers(answers, db_url)
+    data = {"question":[], 
+            "cleaned_answer":[],
+            "category":[] ,
+            "original_answer":[], 
+            'answer_type':[]}
+    for question, cleaned_answer, answer, rep, ans_type in zip(questions, cleaned_answers, answers, replicates, a_types):
+        if rep:
+            continue
+        data['question'].append(question['Question'])
+        data['category'].append(question['Category'])
+        if ans_type != 'coord':
+            data['original_answer'].append(answer)
+        else:
+            # to avoid having a file too fat from unreadable data
+            data['original_answer'].append(None)
+        '''
+        if rep:
+            a = cleaned_answers[int(cleaned_answer)-1]
+            cat = questions[int(cleaned_answer)-1]['Category']
+            a_type = a_types[int(cleaned_answer)-1]
+            print(str(cleaned_answer), cat)
+        else:
+            a = cleaned_answer
+            cat = question['Category']
+            a_type = ans_type
+        '''
+        a = cleaned_answer
+        cat = question['Category']
+        a_type = ans_type
+        data['cleaned_answer'].append(a)
+        data['answer_type'].append(a_type)
+
+    df = pd.DataFrame(data)
+    df.to_csv(os.path.join(output_dir, "dataset.csv"), index=False)
+
+if __name__ == '__main__':
+    args = parse_args()
+    questions = load_json(os.path.join(args.data_dir, "GeoQuestions1089.json"))
+    answers = load_json(os.path.join(args.data_dir, "GeoQuestions1089_answers.json"))
+    
+    create_csv(questions, answers, args.output_dir, args.db_url)
+
+
+
